2fe9ad1c4074dab870bccb8d08d5a2d6f35fdbc2
Armon Dadgar <armon.dadgar@gmail.com>
2014-08-06 15:35:24 -0700

    First pass

---
+++ lru_test.go
@@ -0,0 +1,37 @@
+package lru
+
+import (
+	"testing"
+)
+
+func TestLRU(t *testing.T) {
+	l, err := New(128)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	for i := 0; i < 256; i++ {
+		l.Add(i, i)
+	}
+	if l.Len() != 128 {
+		t.Fatalf("bad len: %v", l.Len())
+	}
+	for i := 0; i < 128; i++ {
+		_, ok := l.Get(i)
+		if ok {
+			t.Fatalf("should be evicted")
+		}
+	}
+	for i := 128; i < 256; i++ {
+		_, ok := l.Get(i)
+		if !ok {
+			t.Fatalf("should not be evicted")
+		}
+	}
+	for i := 128; i < 192; i++ {
+		l.Remove(i)
+		_, ok := l.Get(i)
+		if ok {
+			t.Fatalf("should be deleted")
+		}
+	}
+}
