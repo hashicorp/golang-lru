f702be2c9bdd3ced918a6d21dba24306a70a92df
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2022-11-16 07:07:45 +0900

    Remove patented files (arc.go, arc_test.go)
    This commit ensures that the patented files are not compiled in,
    so that consumers do not need to validate that the patented files are
    not actually used.
    
    Consumers can still opt-in to use the patented files by copying them.
    
    Fix #31
    Fix #73
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- doc.go
+++ doc.go
@@ -8,14 +8,6 @@
 // This avoids a burst of accesses from taking out frequently used entries,
 // at the cost of about 2x computational overhead and some extra bookkeeping.
 //
-// ARCCache is an adaptive replacement cache. It tracks recent evictions as
-// well as recent usage in both the frequent and recent caches. Its
-// computational overhead is comparable to TwoQueueCache, but the memory
-// overhead is linear with the size of the cache.
-//
-// ARC has been patented by IBM, so do not use it if that is problematic for
-// your program.
-//
 // All caches in this package take locks while operating, and are therefore
 // thread-safe for consumers.
 package lru
