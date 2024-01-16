bbc5d78fcc4c6b92226ef0637ea40d9a94d434f1
Venkat <venkatakrishnansvpr@gmail.com>
2024-01-15 17:18:27 -0800

    benchmark tests

--- lru_test.go
+++ lru_test.go
@@ -8,63 +8,94 @@ import (
 	"testing"
 )
 
-func BenchmarkLRU_Rand(b *testing.B) {
-	l, err := New[int64, int64](8192)
-	if err != nil {
-		b.Fatalf("err: %v", err)
+func Benchmark_Rand(b *testing.B) {
+	var fn = func(b *testing.B, l *Cache[int64, int64]) {
+		trace := make([]int64, b.N*2)
+		for i := 0; i < b.N*2; i++ {
+			trace[i] = getRand(b) % 32768
+		}
+
+		b.ResetTimer()
+
+		var hit, miss int
+		for i := 0; i < 2*b.N; i++ {
+			if i%2 == 0 {
+				l.Add(trace[i], trace[i])
+			} else {
+				if _, ok := l.Get(trace[i]); ok {
+					hit++
+				} else {
+					miss++
+				}
+			}
+		}
+
+		b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
 	}
 
-	trace := make([]int64, b.N*2)
-	for i := 0; i < b.N*2; i++ {
-		trace[i] = getRand(b) % 32768
-	}
+	b.Run("Benchmark with LRU ", func(b *testing.B) {
+		l, err := New[int64, int64](8192)
+		if err != nil {
+			b.Fatalf("err: %v", err)
+		}
 
-	b.ResetTimer()
+		fn(b, l)
+	})
 
-	var hit, miss int
-	for i := 0; i < 2*b.N; i++ {
-		if i%2 == 0 {
+	b.Run("Benchmark with Sieve ", func(b *testing.B) {
+		l, err := NewSieve[int64, int64](8192)
+		if err != nil {
+			b.Fatalf("err: %v", err)
+		}
+
+		fn(b, l)
+	})
+}
+
+func BenchmarkLRU_Freq(b *testing.B) {
+	var fn = func(b *testing.B, l *Cache[int64, int64]) {
+		trace := make([]int64, b.N*2)
+		for i := 0; i < b.N*2; i++ {
+			if i%2 == 0 {
+				trace[i] = getRand(b) % 16384
+			} else {
+				trace[i] = getRand(b) % 32768
+			}
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
 			l.Add(trace[i], trace[i])
-		} else {
+		}
+		var hit, miss int
+		for i := 0; i < b.N; i++ {
 			if _, ok := l.Get(trace[i]); ok {
 				hit++
 			} else {
 				miss++
 			}
 		}
-	}
-	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
-}
-
-func BenchmarkLRU_Freq(b *testing.B) {
-	l, err := New[int64, int64](8192)
-	if err != nil {
-		b.Fatalf("err: %v", err)
+		b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
 	}
 
-	trace := make([]int64, b.N*2)
-	for i := 0; i < b.N*2; i++ {
-		if i%2 == 0 {
-			trace[i] = getRand(b) % 16384
-		} else {
-			trace[i] = getRand(b) % 32768
+	b.Run("Benchmark with LRU ", func(b *testing.B) {
+		l, err := New[int64, int64](8192)
+		if err != nil {
+			b.Fatalf("err: %v", err)
 		}
-	}
 
-	b.ResetTimer()
+		fn(b, l)
+	})
 
-	for i := 0; i < b.N; i++ {
-		l.Add(trace[i], trace[i])
-	}
-	var hit, miss int
-	for i := 0; i < b.N; i++ {
-		if _, ok := l.Get(trace[i]); ok {
-			hit++
-		} else {
-			miss++
+	b.Run("Benchmark with Sieve ", func(b *testing.B) {
+		l, err := NewSieve[int64, int64](8192)
+		if err != nil {
+			b.Fatalf("err: %v", err)
 		}
-	}
-	b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(hit+miss))
+
+		fn(b, l)
+	})
 }
 
 func TestLRU(t *testing.T) {
